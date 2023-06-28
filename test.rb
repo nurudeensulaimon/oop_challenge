70573f949a2096630547e799df626719c5836d53
Nurudeen Sulaimon <nerodean@Neros-MacBook-Pro.local>
2023-06-28 17:07:55 -0400

    Challenge Practice

---
+++ test.rb
@@ -0,0 +1,57 @@
+
+
+# names="Nurudeen","Nicholas","trump","Ba","ASh"
+
+# names.each do |name|
+#     if name.length > 3
+#         puts name 
+#     end  
+# end 
+
+
+# Mistake made: I was putting array of string 
+# inside of a bracket 
+
+
+# def string_len_even(strings)
+#     if strings.length % 2 == 0
+#         puts true 
+#     elsif  
+#         puts false 
+#     end 
+# end 
+# string_len_even("Nurudeen")
+
+
+# def string_len_even(strings)
+#     if strings.length.even? 
+#         puts true 
+#     elsif  
+#         puts false 
+#     end 
+# end 
+# stiring_len_even("Nurudeen")
+
+# Start with an array of strings. 
+# Print all of the words in the array, 
+# but change every t to an uppercase T.
+
+#puts "Nurudeen".chr == "z"
+#string.replace("T")
+
+# elements ="Tom", "TAtata","Toto", "tatiana"
+# elements.each do |element|
+#     if element.include? "t"
+#         puts element.gsub("t", "T")
+#     elsif  
+#         puts element 
+#     end 
+# end 
+        
+
+elements="Hector", "Winston", "Finley"
+#puts elements.sort 
+elements.each_with_index do |ind,element|
+puts ind, element
+end 
+
